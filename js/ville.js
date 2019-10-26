15a22360cd01b03c3020fd63a81250973542a05d
AquaBx <tom.chauvel@hotmail.com>
2019-10-26 21:06:34 +0200

    t

--- js/ville.js
+++ js/ville.js
@@ -781,7 +781,7 @@ export var ville = [
   },
   {
     "type": "Feature",
-    "properties": { "secteur": "21", "habitations": [21] },
+    "properties": { "secteur": "21", "habitations": habitations[21] },
     "geometry": {
       "type": "Polygon",
       "coordinates": [
